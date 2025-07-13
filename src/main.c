b3b1019e2d945ea719df5b6a4637927ab42af1da
JeffMuter <muterjeffery@gmail.com>
2025-07-13 02:55:16 -0400

    got a player on the screen.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,23 @@
 #include <raylib.h>
 
 int main() {
-    InitWindow(800, 450, "Hello Raylib");
+
+    typedef struct {
+        int x, y, health;
+    } Player;
+
+    InitWindow(800, 450, "dust_to_dust");
+    ToggleFullscreen();
+    int scrWidth = GetScreenWidth();
+    int scrHeight = GetScreenHeight();
+
+    Player player = {scrWidth / 2, scrHeight / 2, 100};
     
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+        DrawRectangle(player.x, player.y, 30, 30, RED);
+
         EndDrawing();
     }
     
